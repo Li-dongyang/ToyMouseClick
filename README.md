e2b7047fa88f1fe32ea917d38d13b066ebcb2fd6
Li Ruixiao <80368770+Li-dongyang@users.noreply.github.com>
2024-04-26 13:22:33 +0000

    readme

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # ToyMouseClick
-基于 python 的极简连点器，使用 F9 控制启动和停止。
+基于 python 的极简连点器，使用 F9 控制启动和停止。修改 `main.py` 中的 `CPS` 变量来控制连点频率
+
+A minimalist Python-based clicker, controlled by the `F9` key for start and stop. Modify the `CPS` variable within `main.py` to adjust the clicking frequency.
--- main.py
+++ main.py
@@ -2,20 +2,23 @@ from pynput.mouse import Button, Controller
 import keyboard
 import time
 
+CPS = 25 # click per second
+
 clicking = False
 
 def toggle_clicking():
     global clicking
     clicking = not clicking
     if clicking:
-        print('clicking ---', flush=True)
+        print('clicking ...', flush=True)
+    else:
+        print('... inactive', flush=True)
 
 mouse = Controller()
 # mouse.position = (10, 20) 设置鼠标位置
 keyboard.add_hotkey('F9', toggle_clicking) # this is a callback function
-# keyboard.add_hotkey('F10', toggle_clicking)
 
 while True:
     if clicking:
         mouse.click(Button.left, 1) # 单击
-        time.sleep(0.05)  # 控制点击速度
+        time.sleep(1.0 / CPS)
